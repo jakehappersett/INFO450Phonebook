3ca436ad513a48741b85456b485d06a9d898212b
jakehappersett <jake.happersett@gmail.com>
2016-09-18 22:33:18 -0400

    initial commit

---
+++ phoneBook.cpp
@@ -0,0 +1,85 @@
+#include <iostream> 
+#include <cstring> 
+
+
+using namespace std;
+
+//char phonebook[10][2][50] = {{{'j','a','k','e'},{'8','0','4'}}};
+char pb[10][2][50];
+void addEntry(char arr[10][2][50]);
+void display(char arr[10][2][50]);
+int i = 0;
+int main () {
+	//cout << phonebook[0][0] << endl;
+	//cout << phonebook[0][1][0] << endl;
+	//addEntry(pb);
+	//cout << "number is" << i << endl;
+	//addEntry(pb);
+	//cout << "number is" << i << endl;
+	//display(pb);
+	char opt;
+	do {
+		cout << "--------------------------" << endl;
+		cout << "  what do you want to do  " << endl;
+	        cout << "--------------------------" << endl;
+		cout << " add new contact  --> a   " << endl;
+		cout << " display contacts --> d   " << endl;
+		cout << " quit             --> q   " << endl;
+		cin >> opt;
+		if (opt == 'a' || opt == 'A') 
+		{
+			if (i < 10) {
+
+				cin.ignore(); //clears the input buffer (found this online), allows me to use the cin.getline after a cin 
+				addEntry(pb);
+			}
+			else{
+				cout << "It's full, get less friends!" << endl;
+			}
+		}
+		else if (opt == 'd' || opt =='D')
+		{
+			display(pb);
+		}
+		else if (opt== 'q' || opt == 'Q')
+			break;
+	}while(1==1);
+
+	return 0;
+
+	
+}
+
+void addEntry(char arr[10][2][50])
+{
+	char name[50];
+	cout << "what is the name?" << endl;
+	//cin.getline(arr[i][0], 50);
+	cin.getline(name, 50);
+	//checking for duplicates		
+//	for (int c=0; c <= i; c++)
+//	{
+//		if (name == arr[c][0])
+//		{
+//			cout << "you already did that" << endl;
+//		}
+//		else if (name != arr[c][0] && c==i)
+//		{
+//			arr[i][0] = name;
+//		}
+//	}
+	cout << "what is the number?" << endl;
+	cin.getline(arr[i][1], 50);
+	i++;	
+
+	
+}
+
+void display(char arr[10][2][50])
+{
+
+	for (int a=0; a < i; a ++)
+	{
+		cout << "Entry: " << a+1 << " Name: " << arr[a][0] << " Number: " << arr[a][1] << endl;
+	}
+}
